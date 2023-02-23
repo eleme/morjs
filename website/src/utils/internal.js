cf556864544acfb417d25f6aa30479bffed5a55c
lyfeyaj <lyfeyaj@gmail.com>
2023-02-23 12:54:19 +0800

    docs(website): fix website show redirect links when access from internet

--- website/src/utils/internal.js
+++ website/src/utils/internal.js
@@ -1,22 +1,24 @@
 const EVENT_NAME = 'check:internal'
 let pending = false
 
-export function isIntranet() {
+export async function isIntranet() {
   if (pending === false) {
     pending = true
-    return _checkIntranet().then((validAuthorization) => {
+    _checkIntranet().then((validAuthorization) => {
       if (validAuthorization) {
         window.dispatchEvent(new CustomEvent(EVENT_NAME))
         pending = false
       }
     })
-  } else {
-    return new Promise((resolve) => {
-      window.addEventListener(EVENT_NAME, () => {
-        resolve()
-      })
-    })
   }
+
+  return new Promise((resolve) => {
+    const fullfilled = () => {
+      window.removeEventListener(EVENT_NAME, fullfilled)
+      resolve()
+    }
+    window.addEventListener(EVENT_NAME, fullfilled)
+  })
 }
 
 // 检查是否是内网环境
