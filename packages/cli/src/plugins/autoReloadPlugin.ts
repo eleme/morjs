444d45384cef3bcc9f85eb3251148d0cb9600de5
lyfeyaj <lyfeyaj@gmail.com>
2023-03-17 14:21:48 +0800

    fix(cli): 修复配置变更自动重启可能会触发多次的问题

--- packages/cli/src/plugins/autoReloadPlugin.ts
+++ packages/cli/src/plugins/autoReloadPlugin.ts
@@ -1,6 +1,8 @@
 import { fsExtra, logger, Plugin, Takin } from '@morjs/utils'
+import { createHash } from 'crypto'
 
 let IS_RELOADING = false
+let CONFIG_FILE_HASH = ''
 
 /**
  * 用户配置文件发生修改时自动重新加载
@@ -9,24 +11,38 @@ export default class MorAutoReloadOnConfigChangePlugin implements Plugin {
   name = 'MorAutoReloadOnConfigChangePlugin'
 
   onUse(mor: Takin) {
-    mor.hooks.configLoaded.tap(this.name, function (_, commandOptions) {
+    mor.hooks.configLoaded.tapPromise(this.name, async (_, commandOptions) => {
       // 仅在用户配置文件存在, 且开启了 watch 的情况下自动重载
       const isWatching =
         commandOptions?.options?.watch || commandOptions?.options?.w
 
       if (mor.userConfigFilePath && isWatching) {
+        CONFIG_FILE_HASH = await this.computeFileHash(mor.userConfigFilePath)
+
         logger.debug(`用户配置文件监听中...`, commandOptions)
         const watcher = fsExtra.watch(
           mor.userConfigFilePath,
-          async function (event, fileName) {
+          async (event, fileName) => {
             if (IS_RELOADING) return
+
+            const newFileHash = await this.computeFileHash(
+              mor.userConfigFilePath
+            )
+            if (CONFIG_FILE_HASH === newFileHash) {
+              logger.debug('忽略用户配置变更监听，内容未变化')
+              return
+            }
+
+            CONFIG_FILE_HASH = newFileHash
+
+            IS_RELOADING = true
 
             logger.debug({ fileName, event })
             logger.warn(`配置文件 ${fileName} 发生变更, 自动重启中...\n`)
 
             try {
-              await mor.reload()
               watcher.close()
+              await mor.reload()
             } catch (error) {
               logger.error('自动重启失败, 请尝试手动重启。', { error })
             } finally {
@@ -36,6 +52,11 @@ export default class MorAutoReloadOnConfigChangePlugin implements Plugin {
         )
       }
     })
+  }
+
+  async computeFileHash(filePath: string) {
+    const fileData = (await fsExtra.readFile(filePath)).toString()
+    return createHash('md5').update(fileData).digest('hex')
   }
 
   apply() {
