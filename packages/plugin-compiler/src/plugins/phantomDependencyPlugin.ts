d8a0c04b3ae6678777859e6da2698b1ec15ea1f5
BboyZaki <1512438313@qq.com>
2023-03-28 20:50:47 +0800

    feat(plugin-compiler): compatible with alias configuration

--- packages/plugin-compiler/src/plugins/phantomDependencyPlugin.ts
+++ packages/plugin-compiler/src/plugins/phantomDependencyPlugin.ts
@@ -4,92 +4,140 @@ import {
   Plugin,
   Runner,
   tsTransformerFactory,
-  typescript as ts
+  typescript as ts,
+  WebpackWrapper
 } from '@morjs/utils'
 import path from 'path'
 import { NODE_MODULE_REGEXP } from '../constants'
 
 /**
  * 检测项目中的幽灵依赖
- * 1. 以下情况直接跳过该文件的检测
- *   1.1 生产环境
- *   1.2 phantomDependency 为 false 时
- *   1.3 该文件为 node_modules 文件
- *   1.4 文件不在当前 pwd 运行路径内
- * 2. 获取 import 和 require 的依赖进行检测
- *   2.1 跳过以 . / @/ @@/ 开头的本地文件引用
- *   2.2 跳过已在 package.json 中配置的依赖
- *   2.3 跳过配置在 phantomDependency.exclude 的包
- *   2.4 针对检测模式进行 error | warn(默认)
+ * 以下情况直接跳过该文件的检测
+ * - 生产环境
+ * - phantomDependency 为 false 时
+ * - 该文件为 node_modules 文件
+ * - 文件不在当前 pwd 运行路径内
  */
 export class PhantomDependencyPlugin implements Plugin {
   name = 'PhantomDependencyPlugin'
+  webpackWrapper: WebpackWrapper
 
   apply(runner: Runner<any>) {
-    runner.hooks.scriptParser.tap(this.name, (transformers, options) => {
+    runner.hooks.webpackWrapper.tap(this.name, (webpackWrapper) => {
+      this.webpackWrapper = webpackWrapper
+    })
+
+    const usedDependencies: Record<string, string> = {}
+
+    runner.hooks.beforeRun.tap(this.name, () => {
       const { phantomDependency, mode } = runner.userConfig
-      const fileInfoPath = options.fileInfo.path || ''
-      if (
-        mode === 'production' ||
-        !phantomDependency ||
-        NODE_MODULE_REGEXP.test(fileInfoPath) ||
-        !fileInfoPath.includes(process.cwd())
-      )
+      if (mode === 'production' || !phantomDependency) return
+
+      runner.hooks.scriptParser.tap(this.name, (transformers, options) => {
+        const fileInfoPath = options.fileInfo.path || ''
+        if (
+          !NODE_MODULE_REGEXP.test(fileInfoPath) &&
+          fileInfoPath.includes(process.cwd())
+        ) {
+          this.handlePhantomTransformer(
+            transformers,
+            fileInfoPath,
+            usedDependencies
+          )
+        }
         return transformers
+      })
 
-      transformers.before.push(
-        tsTransformerFactory((node) => {
-          if (!node) return node
+      runner.hooks.sjsParser.tap(this.name, (transformers, options) => {
+        const fileInfoPath = options.fileInfo.path || ''
+        if (
+          !NODE_MODULE_REGEXP.test(fileInfoPath) &&
+          fileInfoPath.includes(process.cwd())
+        ) {
+          this.handlePhantomTransformer(
+            transformers,
+            fileInfoPath,
+            usedDependencies
+          )
+        }
+        return transformers
+      })
+    })
 
-          // 处理 import 节点
-          if (
-            ts.isImportDeclaration(node) &&
-            ts.isStringLiteral(node.moduleSpecifier)
-          ) {
-            const npmName = node.moduleSpecifier?.text
-            this.checkDependency(npmName, fileInfoPath, phantomDependency)
-          }
+    runner.hooks.done.tap(this.name, () => {
+      const { alias, srcPaths, phantomDependency } = runner.userConfig
+      let allDependencies = { ...this.getPkgDepend(process.cwd()) }
+      ;(srcPaths || []).map((srcPath) => {
+        allDependencies = { ...allDependencies, ...this.getPkgDepend(srcPath) }
+      })
 
-          // 处理 require 节点
-          if (
-            ts.isCallExpression(node) &&
-            ts.isIdentifier(node.expression) &&
-            node.expression?.escapedText === 'require' &&
-            ts.isStringLiteral(node.arguments?.[0])
-          ) {
-            const npmName = node.arguments?.[0].text
-            this.checkDependency(npmName, fileInfoPath, phantomDependency)
-          }
-          return node
-        })
-      )
+      // 搜集 userConfig 和 webpack 里的 alias 配置
+      const aliasAll = {
+        ...alias,
+        ...this.webpackWrapper?.config?.resolve?.alias
+      }
 
-      return transformers
+      const table = {
+        head: ['依赖名', '引用地址'],
+        rows: []
+      }
+
+      // 跳过已在 package.json 和 phantomDependency.exclude 中配置的依赖
+      // 跳过 alias 及以 alias 中以 key 开头的依赖包
+      for (const depKey in usedDependencies) {
+        if (
+          !(phantomDependency.exclude || []).includes(depKey) &&
+          !allDependencies[depKey] &&
+          !aliasAll[depKey] &&
+          !aliasAll[depKey.split('/')[0]]
+        ) {
+          table.rows.push([depKey, usedDependencies[depKey]])
+        }
+      }
+
+      if (table.rows.length > 0) {
+        if (phantomDependency.mode === 'error') {
+          logger.error('检测到幽灵依赖')
+        } else {
+          logger.warnOnce('检测到幽灵依赖')
+        }
+        logger.table(table)
+      }
     })
   }
 
-  /**
-   * 检查所引用的依赖是否存在 package.json 里
-   * @param npmName - 使用的 npm 包名
-   * @param pkg - 项目 package.json 配置
-   * @param phantomDependency - config 中的配置项
-   */
-  checkDependency(npmName, fileInfoPath, phantomDependency) {
-    if (/^(\.|\/|@\/|@@\/)/.test(npmName)) return
+  // 检查 script 和 sjs 文件 获取 import 和 require 的依赖进行检测
+  handlePhantomTransformer(transformers, fileInfoPath, usedDependencies) {
+    transformers.before.push(
+      tsTransformerFactory((node) => {
+        if (!node) return node
 
-    const pkgName = this.getPkgName(npmName)
-    const pkgDepends = this.getAllPkgDepends(fileInfoPath)
+        // 处理 import 节点
+        if (
+          ts.isImportDeclaration(node) &&
+          ts.isStringLiteral(node.moduleSpecifier)
+        ) {
+          const pkgName = this.getPkgName(node.moduleSpecifier?.text)
+          // 跳过以 . / @/ @@/ 开头的本地文件引用
+          if (!/^(\.|\/|@\/|@@\/)/.test(pkgName))
+            usedDependencies[pkgName] = fileInfoPath
+        }
 
-    if (pkgDepends[pkgName]) return
-
-    if (
-      Object.prototype.toString.call(phantomDependency) === '[object Object]'
-    ) {
-      if ((phantomDependency.exclude || []).includes(pkgName)) return
-      if (phantomDependency.mode === 'error')
-        throw new Error(`${pkgName} 是一个幽灵依赖，请在 package.json 中添加`)
-    }
-    logger.warnOnce(`${pkgName} 是一个幽灵依赖，建议在 package.json 中添加`)
+        // 处理 require 节点
+        if (
+          ts.isCallExpression(node) &&
+          ts.isIdentifier(node.expression) &&
+          node.expression?.escapedText === 'require' &&
+          ts.isStringLiteral(node.arguments?.[0])
+        ) {
+          const pkgName = this.getPkgName(node.arguments?.[0].text)
+          // 跳过以 . / @/ @@/ 开头的本地文件引用
+          if (!/^(\.|\/|@\/|@@\/)/.test(pkgName))
+            usedDependencies[pkgName] = fileInfoPath
+        }
+        return node
+      })
+    )
   }
 
   /**
@@ -98,32 +146,19 @@ export class PhantomDependencyPlugin implements Plugin {
    */
   getPkgName(source: string) {
     const arr = source.split('/')
-    if (source.startsWith('@')) return arr[0] + '/' + arr[1]
+    if (source.startsWith('@')) return arr[1] ? arr[0] + '/' + arr[1] : arr[0]
     return arr[0]
   }
 
   /**
-   * 获取从当前文件路径逐层往上直至 pwd 的所有 package.json 的依赖
-   * @param source - string 当前文件的路径
+   * 获取指定路径 package.json 内的所有依赖
+   * @param path 路径
    */
-  getAllPkgDepends(fileInfoPath) {
-    let pkgDepends = {}
-    for (
-      let currentPath = fileInfoPath;
-      currentPath !== process.cwd() + '/';
-      currentPath = path.join(currentPath, '../')
-    ) {
-      if (fs.existsSync(path.join(currentPath, '../package.json'))) {
-        const pkgJson = fs.readJSONSync(
-          path.join(currentPath, '../package.json')
-        )
-        pkgDepends = {
-          ...pkgDepends,
-          ...pkgJson.devDependencies,
-          ...pkgJson.dependencies
-        }
-      }
+  getPkgDepend(filePath: string) {
+    if (fs.existsSync(path.join(filePath, 'package.json'))) {
+      const pkgJson = fs.readJSONSync(path.join(filePath, 'package.json'))
+      return { ...pkgJson.devDependencies, ...pkgJson.dependencies }
     }
-    return pkgDepends
+    return {}
   }
 }
