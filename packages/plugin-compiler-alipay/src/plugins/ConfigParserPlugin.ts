b6aa4097c82c16e49f3b018570c59efeacd43df0
lyfeyaj <lyfeyaj@gmail.com>
2023-04-26 13:57:02 +0800

    feat(plugin-compiler-alipay): 完善微信转支付宝的转端兼容性：处理支付宝不支持 sjs 模块名称为 this 及 组件名称不支持大写的问题

--- packages/plugin-compiler-alipay/src/plugins/ConfigParserPlugin.ts
+++ packages/plugin-compiler-alipay/src/plugins/ConfigParserPlugin.ts
@@ -305,6 +305,18 @@ export default class AlipayCompilerConfigParserPlugin implements Plugin {
       this.runner.hooks.configParser.tapPromise(
         this.name,
         async (config, options) => {
+          // 支付宝不支持大写的标签名，这里统一转换为小写
+          if (isAlipaySimilarTarget && config.usingComponents) {
+            for (const componentName in config.usingComponents || {}) {
+              const newComponentName = componentName.toLowerCase()
+              if (newComponentName !== componentName) {
+                config.usingComponents[newComponentName] =
+                  config.usingComponents[componentName]
+                delete config.usingComponents[componentName]
+              }
+            }
+          }
+
           // plugin.json 转换
           config = this.transformPluginJson(config, options)
 
