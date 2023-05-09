6d1298c1cac20a3c14e41f92e054e197e5e53ab9
hwaphon <ghwaphon@gmail.com>
2023-05-09 16:11:07 +0800

    fix(runtime-web): 业务中 onAppear 在某些特殊场景下丢失

--- packages/runtime-web/src/components/src/views/view/index.ts
+++ packages/runtime-web/src/components/src/views/view/index.ts
@@ -29,6 +29,10 @@ export default class View extends BaseElement {
   }
 
   private needBindAppear = false
+  // 标记是否已经触发 firstUpdated 生命周期
+  private isFirstUpdatedInvoked = false
+  // 标记是否已经监听了滚动事件
+  private isWatchTouchMove = false
   private listener = null
 
   addEventListener(type, callback, options?) {
@@ -38,6 +42,13 @@ export default class View extends BaseElement {
       case 'firstappear':
       case 'appear': {
         this.needBindAppear = true
+        /*
+         * 正常情况下是 addEventListener 先执行，然后在 firstUpdated 中绑定事件用于支持 appear，disappear 等功能。
+         * 某些业务场景会先加载骨架屏占位，且占位元素和实际元素使用了相同的 id，React 在这种场景下会复用元素，会先触发 firstUpdated，
+         * 在实际元素渲染的时候再触发 addEventListener。这种特殊的场景就导致 appear，disappear 功能异常，所以需要做如下兼容
+         * 需要通过 isFirstUpdatedInvoked 判断元素是否已经挂载，否则调用 watchTouchMove 会报错
+         */
+        if (this.isFirstUpdatedInvoked) this.watchTouchMove()
         break
       }
     }
@@ -45,6 +56,7 @@ export default class View extends BaseElement {
 
   firstUpdated() {
     requestAnimationFrame(() => {
+      this.isFirstUpdatedInvoked = true
       // firstUpdated 触发时，addEventListener 方法不一定会收到组件注册的 appear，disappear 事件，根据lit官方提示，做延迟处理
       if (this.needBindAppear) {
         this.watchTouchMove()
@@ -63,6 +75,10 @@ export default class View extends BaseElement {
   private hasAppeared = false //是否已经显示过
 
   watchTouchMove() {
+    // 防止重复监听，导致多次触发事件
+    if (this.isWatchTouchMove) return
+    this.isWatchTouchMove = true
+
     const parent: Element = this.getScrollParent()
     const callback = (ratio) => {
       if (ratio >= 0.5 && this.lastTrigger !== 1) {
