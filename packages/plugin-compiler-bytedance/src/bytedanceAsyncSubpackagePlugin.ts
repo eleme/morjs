5f9a8bc7657eba09271df57d758935ed8e5f3b35
BboyZaki <1512438313@qq.com>
2023-05-31 17:34:37 +0800

    feat(plugin-compiler-bytedance): 替换变量判断条件为对应的类型

--- packages/plugin-compiler-bytedance/src/bytedanceAsyncSubpackagePlugin.ts
+++ packages/plugin-compiler-bytedance/src/bytedanceAsyncSubpackagePlugin.ts
@@ -1,4 +1,11 @@
-import { CompileTypes, Plugin, Runner } from '@morjs/utils'
+import {
+  CompileTypes,
+  EntryBuilderHelpers,
+  Plugin,
+  Runner,
+  UserConfig
+} from '@morjs/utils'
+import { target } from './constants'
 
 /**
  * 抖音支持异步分包插件
@@ -14,37 +21,40 @@ export class BytedanceAsyncSubpackagePlugin implements Plugin {
 
   runAsyncSubpackage() {
     const runner = this.runner
-    runner.hooks.userConfigValidated.tap(this.name, (userConfig) => {
-      const { target, compileType } = userConfig
-      if (
-        target === 'bytedance' &&
-        (compileType === CompileTypes.miniprogram ||
-          compileType === CompileTypes.subpackage)
-      ) {
-        runner.hooks.beforeBuildEntries.tapPromise(
-          this.name,
-          async (entryBuilder) => {
-            if (
-              compileType === CompileTypes.subpackage &&
-              entryBuilder?.subpackageJson?.common === true
-            ) {
-              // 分包项目场 且配置了 common: true 时执行
-              await entryBuilder.buildByGlob()
-            } else if (compileType === 'miniprogram') {
-              // 小程序项目 部分分包配置了 common: true 时执行
-              const subpackagesArr =
-                entryBuilder?.appJson?.subpackages ||
-                entryBuilder?.appJson?.subPackages ||
-                []
-              for await (const item of subpackagesArr) {
-                if (item.common === true && item.root) {
-                  await entryBuilder.buildByGlob(`${item.root}/**`)
+    runner.hooks.userConfigValidated.tap(
+      this.name,
+      (userConfig: UserConfig) => {
+        const { target: compileTarget, compileType } = userConfig
+        if (
+          compileTarget === target &&
+          (compileType === CompileTypes.miniprogram ||
+            compileType === CompileTypes.subpackage)
+        ) {
+          runner.hooks.beforeBuildEntries.tapPromise(
+            this.name,
+            async (entryBuilder: EntryBuilderHelpers) => {
+              if (
+                compileType === CompileTypes.subpackage &&
+                entryBuilder?.subpackageJson?.common === true
+              ) {
+                // 分包项目场 且配置了 common: true 时执行
+                await entryBuilder.buildByGlob()
+              } else if (compileType === CompileTypes.miniprogram) {
+                // 小程序项目 部分分包配置了 common: true 时执行
+                const subpackagesArr =
+                  entryBuilder?.appJson?.subpackages ||
+                  entryBuilder?.appJson?.subPackages ||
+                  []
+                for await (const item of subpackagesArr) {
+                  if (item.common === true && item.root) {
+                    await entryBuilder.buildByGlob(`${item.root}/**`)
+                  }
                 }
               }
             }
-          }
-        )
+          )
+        }
       }
-    })
+    )
   }
 }
