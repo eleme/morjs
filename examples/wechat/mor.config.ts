64d4b626f9684232366b868278e7e2c75cd57878
lyfeyaj <lyfeyaj@gmail.com>
2023-06-01 19:01:17 +0800

    docs(examples): 完善微信 DSL 样式以及兼容性

--- examples/wechat/mor.config.ts
+++ examples/wechat/mor.config.ts
@@ -1,4 +1,36 @@
-import { defineConfig } from '@morjs/cli'
+import { defineConfig, logger, Plugin, Runner } from '@morjs/cli'
+
+/**
+ * 插件示例，简单处理示例项目中微信 DSL 转支付宝不兼容的地方
+ */
+class TransformUnsupportTagForAlipayPlugin implements Plugin {
+  name = 'transformUnsupportTagPlugin'
+  apply(runner: Runner) {
+    const unsupportedTags = ['i', 'span', 'img', 'h2', 'strong', 'em', 'a']
+    runner.hooks.templateParser.tap(this.name, function (tree) {
+      return tree.walk((node) => {
+        if (!node.tag || !node.attrs) return node
+        const attrs = node.attrs
+
+        // 示例项目中出现了 html 元素，这里简单做下兼容
+        if (unsupportedTags.includes(node.tag as string)) {
+          attrs.class = attrs.class
+            ? [attrs.class, node.tag].join(' ')
+            : node.tag
+          node.tag = 'view'
+        }
+        // 支付宝 label 不支持 tap 事件
+        if (node.tag === 'label' && attrs.bindtap) {
+          logger.warnOnce(
+            `支付宝 label 标签不支持 tap 事件，将替换为 view 标签`
+          )
+          node.tag = 'view'
+        }
+        return node
+      })
+    })
+  }
+}
 
 export default defineConfig([
   /**
@@ -15,7 +47,8 @@ export default defineConfig([
   {
     name: 'alipay',
     sourceType: 'wechat',
-    target: 'alipay'
+    target: 'alipay',
+    plugins: [new TransformUnsupportTagForAlipayPlugin()]
   },
   /**
    * 微信小程序转字节小程序编译配置
