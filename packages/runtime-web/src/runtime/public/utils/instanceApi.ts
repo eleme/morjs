96f4c34c8ca6147e4ea090bb7c0be1fcb02b471d
Hwaphon <ghwaphon@gmail.com>
2023-08-01 19:58:48 +0800

    feat(runtime-web): 增加 this.createIntersectionObserver 实现 (#92)
    * feat(runtime-web): 增加 this.createIntersectionObserver 实现
    
    * feat(runtime-web): 增加 this.createIntersectionObserver 实现
    
    * feat(runtime-web): 增加 this.createIntersectionObserver 实现
    
    * feat(runtime-web): 补充类型声明

---
+++ packages/runtime-web/src/runtime/public/utils/instanceApi.ts
@@ -0,0 +1,26 @@
+import { my } from '../../../api/my'
+import { IntersectionObserverOptions } from '../../../api/ui/element-query/intersection-observer'
+
+export function mountIntersectionObserver(
+  options: IntersectionObserverOptions,
+  root: Element | Text | null
+) {
+  const intersectionObserver = my.createIntersectionObserver(options)
+  // 保存一份引用，且固定 this 绑定
+  const observe = intersectionObserver.observe.bind(intersectionObserver)
+
+  intersectionObserver.observe = (targetSelector, callback) => {
+    try {
+      let element = null
+      if (root && root.parentNode)
+        element = root.parentNode.querySelector(targetSelector)
+      // 在这种场景下，查找元素要从当前自定义节点下查找，不能从全局角度查找
+      if (element) {
+        return observe(element, callback)
+      }
+    } catch (e) {}
+    // 处理报错了，或者没有正确的获取到 element 元素，走兜底逻辑，全局查询节点
+    return observe(targetSelector, callback)
+  }
+  return intersectionObserver
+}
