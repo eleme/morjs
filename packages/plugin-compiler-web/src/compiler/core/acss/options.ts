db68eecaef0501962ecd3f5c051a8cf5f934603d
冉冉 <1015307548@qq.com>
2024-04-26 14:32:12 +0800

    fix(plugin-compiler-web): 样式文件中@import内容可写入样式文件

--- packages/plugin-compiler-web/src/compiler/core/acss/options.ts
+++ packages/plugin-compiler-web/src/compiler/core/acss/options.ts
@@ -4,4 +4,5 @@ export interface AcssOptions extends BuildOptions {
   plugins?: string[]
   needRpx?: boolean // 是否需要rpx 转换。默认true
   syntax?: any // 自定义语法处理程序
+  enableCombineImportStyles?: boolean // 是否需要解析@import导入的样式
 }
