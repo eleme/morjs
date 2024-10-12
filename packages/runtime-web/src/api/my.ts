b86809e4d36e5dc821196c79127bff7151676251
jiwei.bjw <jiwei.bjw@alibaba-inc.com>
2024-10-12 13:42:58 +0800

    feat(runtime-web): 优化 window[globalKey] 挂载方式，使其不能被覆盖

--- packages/runtime-web/src/api/my.ts
+++ packages/runtime-web/src/api/my.ts
@@ -15,10 +15,43 @@ export function getGlobalObject() {
   return globalKey
 }
 
+// window.my = {
+//   aaaa: 1,
+//   bbb: () => {}
+// }
+
 const globalKey = getGlobalObject()
-window[globalKey] = window[globalKey] || {
+
+const morMy = window[globalKey] || {
   // 这里拷贝下原始的 window.my 中提供的方法
   ...(window?.[DEFAULT_GLOBAL_OBJECT] || {})
 }
+
+if (window[DEFAULT_GLOBAL_OBJECT]) {
+  window.originalMy = {}
+  const myKeys = Object.keys(window[DEFAULT_GLOBAL_OBJECT])
+  for (let i = 0; i < myKeys.length; i++) {
+    const key = myKeys[i]
+    window.originalMy[key] = window[DEFAULT_GLOBAL_OBJECT][key]
+  }
+}
+
+// 这里使用 defineProperty 来实现，避免 window[globalKey] 被覆盖
+Object.defineProperty(window, globalKey, {
+  get() {
+    return morMy
+  },
+  set(value) {
+    if (!window.originalMy) {
+      window.originalMy = value
+    }
+    return
+  }
+})
+
+// window[globalKey] = window[globalKey] || {
+//   // 这里拷贝下原始的 window.my 中提供的方法
+//   ...(window?.[DEFAULT_GLOBAL_OBJECT] || {})
+// }
 
 export const my: Record<string, any> = window[globalKey]
