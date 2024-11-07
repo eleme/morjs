68fdec8c35e969cadd438a3108658a5989b56838
jiwei.bjw <jiwei.bjw@alibaba-inc.com>
2024-11-07 22:11:29 +0800

    feat(runtime-web): 优化 window[globalKey] 挂载方式

--- packages/runtime-web/src/api/my.ts
+++ packages/runtime-web/src/api/my.ts
@@ -15,48 +15,34 @@ export function getGlobalObject() {
   return globalKey
 }
 
-// window.my = {
-//   aaaa: 1,
-//   bbb: () => {}
-// }
-
 const globalKey = getGlobalObject()
 const morMy = window[globalKey] || {}
 
-function mergeGlobalObject() {
+if (window[DEFAULT_GLOBAL_OBJECT] && DEFAULT_GLOBAL_OBJECT === globalKey) {
   window.originalMy = {}
   const myKeys = Object.keys(window[DEFAULT_GLOBAL_OBJECT])
   for (let i = 0; i < myKeys.length; i++) {
     const key = myKeys[i]
     window.originalMy[key] = window[DEFAULT_GLOBAL_OBJECT][key]
   }
-  window[globalKey] = window[globalKey] || {
-    // 这里拷贝下原始的 window.my 中提供的方法
-    ...(window?.[DEFAULT_GLOBAL_OBJECT] || {})
-  }
 }
 
-// 小程序 web-view sdk 注入的 window.my configurable 为 false， 不能再 defineProperty 了，如果已经注入，直接覆盖
-if (window[DEFAULT_GLOBAL_OBJECT]) {
-  mergeGlobalObject()
-} else {
-  // 如果小程序 web-view sdk 还没有注入 window.my
-  // 这里使用 defineProperty 来实现，避免 window[globalKey] 被覆盖
-  try {
-    Object.defineProperty(window, globalKey, {
-      get() {
-        return morMy
-      },
-      set(value) {
-        if (!window.originalMy) {
-          window.originalMy = value
-        }
-        return
-      }
-    })
-  } catch (e) {
-    mergeGlobalObject()
-  }
+// 如果小程序 web-view sdk 还没有注入 window.my
+// 这里使用 defineProperty 来实现，避免 window[globalKey] 被覆盖
+try {
+  Object.defineProperty(window, globalKey, {
+    get() {
+      return morMy
+    },
+    set(value) {
+      // if (!window.originalMy) {
+      //   window.originalMy = value
+      // }
+      // return
+    }
+  })
+} catch (e) {
+  window[globalKey] = morMy
 }
 
 // window[globalKey] = window[globalKey] || {
