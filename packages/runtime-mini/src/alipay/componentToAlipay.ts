9ea90b82521425ea05a41441c7736a105cb892b5
huaizhi.xq <huaizhi.xq@alibaba-inc.com>
2025-02-25 10:59:17 +0800

    fix: wx => ali component2 observers

--- packages/runtime-mini/src/alipay/componentToAlipay.ts
+++ packages/runtime-mini/src/alipay/componentToAlipay.ts
@@ -305,6 +305,7 @@ function hackSetData() {
 function injectPropertiesAndObserversSupport(options: Record<string, any>) {
   // 如果支付宝小程序基础库已支持 observers 且用户未手动关闭 observers
   // 则直接自动启用 observers 监听器代替 MorJS 本身的实现逻辑
+  const _observers = options.observers || {}
   if (isObserversSupported && options.options.observers !== false) {
     options.options.observers = true
   }
@@ -420,7 +421,6 @@ function injectPropertiesAndObserversSupport(options: Record<string, any>) {
         this.properties[prop] = _propValue
         this.data[prop] = _propValue
       }
-
       // 微信端组件初始化、属性改变时，会触发属性监听器 property.observer。
       // 而支付宝 deriveDataFromProps 方法，初始化时 this.props 和 nextProps
       // 中属性值相同，不满足 isPropChanged = true
@@ -471,6 +471,21 @@ function injectPropertiesAndObserversSupport(options: Record<string, any>) {
     // 执行原函数
     if (originalDeriveDataFromProps)
       originalDeriveDataFromProps.call(this, nextProps)
+  }
+  if (options.options.observers) {
+    Object.keys(_observers).forEach((key) => {
+      const preFn = _observers[key]
+      _observers[key] = function (...args) {
+        if (this.__createdEmitCallbacks__) {
+          // 创建时： 放到 created之后
+          this.__createdEmitCallbacks__[key] = () => preFn.apply(this, args)
+        } else {
+          // 更新时：直接调用，在didUpdate之前
+          preFn.apply(this, args)
+        }
+      }
+    })
+    options.observers = _observers
   }
 }
 
